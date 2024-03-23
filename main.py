56c34409ce4352c1dbc13bf894e5d6dc35b7e790
Mbaoma <mmaryraphaella@gmail.com>
2024-03-23 17:23:04 +0100

    sca knowledge session dry run

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World", "number": "+234000333888"}
+    return {"message": "Hello World", "number": "+234000333888", "text": "hi"}
